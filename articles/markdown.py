393adb2a8675cdf4bcd8e0d792790f7b02b89417
Tomasz Martin <tomasz.martin@gmail.com>
2018-09-16 17:04:20 +0200

    Working on markdown conversion

---
+++ articles/markdown.py
@@ -0,0 +1,27 @@
+"""Module for converting html into markdown."""
+from bs4 import BeautifulSoup
+
+
+def find_images():
+    pass
+
+
+def find_links():
+    pass
+
+
+def find_bold_text():
+    pass
+
+
+def find_italic_text():
+    pass
+
+
+def find_headers():
+    pass
+
+
+def find_lists():
+    pass
+
---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+@pytest.fixture
+def sample_html():
+    return """
+    <html>
+    <p><strong>Bold text.</strong></span></p>
+    <h1>Header 1</h1>
+    <h2>Header 2</h2>
+    <h3>Header 3</h3>
+    
+    <p>List:</p>
+    <ul>
+    <li><strong>Bold list</li>
+    <li>Regular list</li>
+    </ul>
+    
+    <p><a href="/"><img src="https://sample/img.jpg" alt="alt"/></a></p>
+    
+    </html>
+    """
