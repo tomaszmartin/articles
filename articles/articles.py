aaa5901ec5e0e90f12baff511656bc46f2f07d6a
Tomasz Martin <tomasz.martin@gmail.com>
2018-09-16 15:30:56 +0200

    Reorganizing structure

--- articles/articles.py
+++ articles/articles.py
@@ -4,3 +4,14 @@ import requests
 def extract_article(uri):
     """Extract the title and content from the web page."""
     content = requests.get(uri).content
+    head = extract_head(content)
+    body = extract_body(content)
+    return head, body
+
+
+def extract_head(content):
+    return None
+
+
+def extract_body(content):
+    return None
