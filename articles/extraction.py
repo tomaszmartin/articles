509841e12c7ec7433af75b11ddc29640a86b6f0f
Tomasz Martin <tomasz.martin@gmail.com>
2018-09-29 21:44:40 +0200

    Refactored for pure regex

--- articles/extraction.py
+++ articles/extraction.py
@@ -1,29 +1,29 @@
 """Extracting articles from web pages."""
-import requests
-from bs4 import BeautifulSoup
+import re
+
+FLAGS = re.MULTILINE | re.IGNORECASE | re.DOTALL
 
 
-def extract_article(content):
+def extract_article(html):
     """Extract the title and content from the web page."""
-    head = extract_head(content)
-    body = extract_body(content)
+    head = extract_head(html)
+    body = extract_body(html)
     return head, body
 
 
-def extract_head(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    header = soup.find('h1')
-    return header.text
+def extract_head(html):
+    h1 = re.compile(r'<h1>(.*)</h1>', FLAGS)
+    head = re.search(h1, html)
+    if head:
+        return head.groups()[0]
+
+    return None
 
 
-def extract_body(content):
-    soup = BeautifulSoup(content, 'html.parser')
-    body = soup.find('div', {'class': 'article-main'})
-    return body.text
+def extract_body(html):
+    article = re.compile(r'<div[^>]*class="article-main[^>]>(.*)</div>', FLAGS)
+    body = re.search(article, html)
+    if body:
+        return body.groups()[0]
 
-
-if __name__ == '__main__':
-    uri = 'https://www.spidersweb.pl/2018/09/nintendo-switch-direct-wrzesien.html'
-    content = requests.get(uri).content
-    print(extract_head(content))
-    print(extract_body(content))
+    return None
