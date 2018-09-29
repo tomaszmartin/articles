7a1a5a9ea272928957da9455f33c5aa70f9d6544
Tomasz Martin <tomasz.martin@gmail.com>
2018-09-29 14:41:47 +0200

    Refactoring

--- articles/markdown.py
+++ articles/markdown.py
@@ -2,31 +2,31 @@
 import re
 
 
-def find_images(content):
+def parse_images(content):
     pattern = '<img[^>]+src="(.*)"[^>]+alt="(.*)"/>'
     return re.sub(pattern, r"![\2](\1)", content)
 
 
-def find_links(content):
+def parse_links(content):
     pattern = '<a[^>]+href="(.*)">(.*)</a>'
     return re.sub(pattern, r"[\2](\1)", content)
 
 
-def find_bold_text(content):
+def parse_bolds(content):
     patterns = ['<b[^>]*>(.*)</b>', '<strong[^>]*>(.*)</strong>']
     for pattern in patterns:
         content = re.sub(pattern, r"**\1**", content)
     return content
 
 
-def find_italic_text(content):
+def parse_italics(content):
     patterns = ['<i[^>]*>(.*)</i>', '<em[^>]*>(.*)</em>']
     for pattern in patterns:
         content = re.sub(pattern, r"*\1*", content)
     return content
 
 
-def find_headers(content):
+def parse_headers(content):
     patterns = {'\n#': '<h1[^>]*>(.*)</h1>', '\n##': '<h2[^>]*>(.*)</h2>',
                 '\n###': '<h3[^>]*>(.*)</h3>', '\n####': '<h4[^>]*>(.*)</h4>'}
     for mark, pattern in patterns.items():
--- tests/markdown_test.py
+++ tests/markdown_test.py
@@ -3,47 +3,48 @@ from articles import markdown
 
 def test_markdown_images():
     html = '<img src="https://sample/img.jpg" alt="text"/>'
-    result = markdown.find_images(html)
+    result = markdown.parse_images(html)
     md = '![text](https://sample/img.jpg)'
     assert result == md
 
 
 def test_markdown_links():
     html = '<p>sample <a href="/">link</a></p>'
-    result = markdown.find_links(html)
+    result = markdown.parse_links(html)
     md = '<p>sample [link](/)</p>'
     assert result == md
 
 
 def test_markdown_bold():
     html = '<div>sample <b>bold</b> <strong> again </strong></div>'
-    result = markdown.find_bold_text(html)
+    result = markdown.parse_bolds(html)
     md = '<div>sample **bold** ** again **</div>'
     assert result == md
 
 
 def test_markdown_italic():
     html = '<div>sample <i>bold</i> <em> again </em></div>'
-    result = markdown.find_italic_text(html)
+    result = markdown.parse_italics(html)
     md = '<div>sample *bold* * again *</div>'
     assert result == md
 
 
 def test_markdown_header():
     html = '<h1>h1</h1>'
-    result = markdown.find_headers(html)
+    result = markdown.parse_headers(html)
     md = '\n# h1\n'
     assert result == md
 
 
 def test_markdown_headers():
     html = '<h1>h1</h1><h2>h2</h2>'
-    result = markdown.find_headers(html)
+    result = markdown.parse_headers(html)
     md = '\n# h1\n\n## h2\n'
     assert result == md
 
+
 def test_headers_add_newlines():
     html = 'sample <h1>h1</h1> sth'
-    result = markdown.find_headers(html)
+    result = markdown.parse_headers(html)
     md = 'sample \n# h1\n sth'
     assert result == md
