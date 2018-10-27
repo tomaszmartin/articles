f59b453aae2be1ef0695865a6aae728f2eb2f3f8
Tomasz Martin <tomasz.martin@gmail.com>
2018-10-27 12:06:48 +0200

    Refactored for advanced tags

--- articles/markdown.py
+++ articles/markdown.py
@@ -1,6 +1,21 @@
 """Creating markdown text from raw html"""
 from html.parser import HTMLParser
 
+sample = '<html>\n' \
+           '<p><span><strong>Bold text.</strong></span></p>\n' \
+           '<h1>Header 1</h1>\n' \
+           '<h2>Header 2</h2>\n' \
+           '<h3>Header 3</h3>\n' \
+           '<p>Regular line\n' \
+           'Another line\n</p>' \
+           '<p>List:</p>\n' \
+           '<ul>\n' \
+           '<li><strong>Bold list</strong></li>\n' \
+           '<li>Regular list</li>\n' \
+           '</ul>\n' \
+           '<p><img src="https://sample.com/img.jpg" alt="text"/></p>\n' \
+           '<p><a href="/">link</a></p>\n' \
+           '</html>'
 
 class MarkdownParser(HTMLParser):
     """Parses html and creates markdown of of it"""
@@ -17,33 +32,34 @@ class MarkdownParser(HTMLParser):
         'strong': ('**', '**'),
         'b': ('**', '**'),
         'blockquote': ('>', ''),
-        'q': ('>', ''),
+        'q': ('> ', ''),
     }
     __to_skip = [
         'style',
         'script',
     ]
+    __to_extract = {
+        'a': '[{data}]({link})'
+    }
 
     def __init__(self, *args, **kwargs):
         HTMLParser.__init__(self, *args, **kwargs)
-        self.skip = False
-        self.md = ''
+        self.content = ''
+        self.open_tags = []
+        self.tree = []
 
     def handle_starttag(self, tag, attrs):
-        sign = self.__to_replace.get(tag, self.__defaults)[0]
-        self.md += sign
-        if tag in self.__to_skip:
-            self.skip = True
-
-    def handle_endtag(self, tag):
-        sign = self.__to_replace.get(tag, self.__defaults)[1]
-        self.md += sign
-        if tag in self.__to_skip:
-            self.skip = False
+        self.open_tags.append({'name': tag, 'attrs': attrs})
 
     def handle_data(self, data):
-        if not self.skip:
-            self.md += data
+        self.open_tags[-1]['data'] = data
+
+    def handle_endtag(self, tag):
+        data = self.open_tags.pop()
+        self.convert_tag(data)
+
+    def convert_tag(self, tag):
+        print(tag)
 
 
 class Markdown:
@@ -58,5 +74,9 @@ class Markdown:
     def content(self):
         if not self._raw:
             self.parser.feed(self.html)
-            self._raw = self.parser.md
+            self._raw = self.parser.content
         return self._raw
+
+
+md = Markdown(sample)
+print(md.content)
